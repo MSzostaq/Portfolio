dd2f42aad0b2a0ce6432cace2714b7eebc244a7c
MSzostaq <maciek.szostak55@gmail.com>
2021-08-18 15:50:05 +0200

    added views to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import About from "views/About";
+import Contact from "views/Contact";
+import Welcome from "views/Welcome";
+import Navbar from "components/Navbar";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 const App = () => {
-  return <p>This will be a portfolio! Stay tuned :D</p>;
+  return (
+    <Wrapper>
+      <Navbar />
+      <Welcome />
+      <About />
+      <Contact />
+    </Wrapper>
+  );
 };
 
 export default App;

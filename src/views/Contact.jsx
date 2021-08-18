9be13ae0b88cb86b2132bedc2721f460380b3443
MSzostaq <maciek.szostak55@gmail.com>
2021-08-18 15:49:41 +0200

    added id and background-color to Contact view

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import styled from "styled-compoennts";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
+  background-color: #f5f5f5;
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 100vh;
 `;
 
 const Contact = () => {
   return (
-    <Wrapper>
+    <Wrapper id="contact">
       <h1>Contact</h1>
     </Wrapper>
   );

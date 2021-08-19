005bba8a53ab03c7817c72b678fc44bef37644fe
MSzostaq <maciek.szostak55@gmail.com>
2021-08-19 08:13:55 +0200

    changed background color

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background-color: #f5f5f5;
+  background-color: ${({ theme }) => theme.colors.succes};
   display: flex;
   align-items: center;
   justify-content: center;

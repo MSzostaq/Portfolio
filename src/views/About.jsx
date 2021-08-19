46525b5d7025e61ba9fb8281cd5c5cb20e0aa34e
MSzostaq <maciek.szostak55@gmail.com>
2021-08-19 09:37:36 +0200

    About- added contend and some styled

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -6,13 +6,40 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   height: 100vh;
+`;
+
+const Title = styled.p`
+  color: ${({ theme }) => theme.colors.darkGrey};
+  font-family: sans-serif;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+`;
+
+const Content = styled.ul`
+  margin: 8px;
+`;
+
+const Item = styled.li`
+  color: ${({ theme }) => theme.colors.darkGrey};
+  font-family: sans-serif;
+  font-size: ${({ theme }) => theme.fontSize.l};
+  padding: 0 2px;
 `;
 
 const About = () => {
   return (
     <Wrapper id="about">
-      <h1>About</h1>
+      <Title>Known technologies</Title>
+      <Content>
+        <Item>React.js</Item>
+        <Item>Redux</Item>
+        <Item>JavaScript (also ES6 features)</Item>
+        <Item>HTML</Item>
+        <Item>CSS + styled-components</Item>
+        <Item>Git + GitHub</Item>
+        <Item>Basics of TypeScript</Item>
+      </Content>
     </Wrapper>
   );
 };

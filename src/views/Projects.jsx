4f5e0656e1e9bec51ad551d51ee90f3abedc1bf1
MSzostaq <maciek.szostak55@gmail.com>
2021-08-24 11:03:31 +0200

    Projects - grid changes

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.blue};
+  background-color: ${({ theme }) => theme.colors.js};
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   height: 100vh;
 `;
 
@@ -15,10 +16,26 @@ const Title = styled.p`
   font-size: ${({ theme }) => theme.fontSize.l};
 `;
 
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ProjectCard = styled.div`
+  background-color: ${({ theme }) => theme.colors.darkGrey};
+  margin: 8px;
+  position: absolute;
+  width: 200px;
+  height: 20vh;
+`;
+
 const Contact = () => {
   return (
     <Wrapper id="projects">
       <Title>Check out my projects!</Title>
+      <Content>
+        <ProjectCard />
+      </Content>
     </Wrapper>
   );
 };

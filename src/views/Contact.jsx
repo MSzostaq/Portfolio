8624909c55918d838712e5001a0be376f4192586
MSzostaq <maciek.szostak55@gmail.com>
2021-08-24 11:03:23 +0200

    Contact - grid changes

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import Icon from "components/Icon";
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.succes};
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   height: 100vh;
 `;
 
@@ -15,10 +18,41 @@ const Title = styled.p`
   font-size: ${({ theme }) => theme.fontSize.l};
 `;
 
+const Content = styled.ul`
+  display: flex;
+  margin: 8px;
+`;
+
+const IconAnimation = styled(motion.div)`
+  background-color: transparent;
+`;
+
+const StyledGithubIcon = styled(Icon)`
+  color: ${({ theme }) => theme.colors.darkGrey};
+  margin: 8px;
+  width: 64px;
+  height: 64px;
+`;
+
+const StyledLinkedInIcon = styled(Icon)`
+  color: ${({ theme }) => theme.colors.linkedIn};
+  margin: 8px;
+  width: 64px;
+  height: 64px;
+`;
+
 const Contact = () => {
   return (
     <Wrapper id="contact">
-      <Title>Contact me via email, LinkedIn and phone. Enjoy!</Title>
+      <Title>Contact me via email, gitHub, LinkedIn. Enjoy!</Title>
+      <Content>
+        <IconAnimation whileHover={{ scale: 1.1 }}>
+          <StyledGithubIcon icon="github_64" />
+        </IconAnimation>
+        <IconAnimation whileHover={{ scale: 1.1 }}>
+          <StyledLinkedInIcon icon="linkedin_64" />
+        </IconAnimation>
+      </Content>
     </Wrapper>
   );
 };

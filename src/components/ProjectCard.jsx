5237d668dd0adf0a990d17a539fc50bd2a9b8956
MSzostaq <maciek.szostak55@gmail.com>
2021-09-11 08:49:30 +0200

    changed background-color and size of ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 const ProjectCard = styled.div`
-  background-color: rgba(0, 0, 102, 0.6);
+  background-color: rgba(0, 0, 102, 0.3);
   border-radius: 8px;
   margin: 4px;
   width: 70vw;
-  height: 28vh;
+  height: 26vh;
 
   @media (min-width: 800px) {
     width: 30vw;
-    height: 60vh;
+    height: 40vh;
+    margin: 8px;
   }
 `;
 

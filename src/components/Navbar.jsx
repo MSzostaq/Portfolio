e463b09ce8dade522bbf35f12d3bda57556f9fcc
MSzostaq <maciek.szostak55@gmail.com>
2021-10-14 18:50:01 +0200

    added margins

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,11 +17,11 @@ const StyledNavbar = styled.div`
   height: 56px;
 `;
 
-const LinkWrapper = styled.ul``;
+const LinkWrapper = styled.ul`
+  margin-left: 8px;
+`;
 
 const StyledLink = styled.a`
-  ${({ active, theme }) =>
-    active && `border-bottom: 2px solid ${theme.colors.js}`};
   background-color: transparent;
   color: ${({ theme }) => theme.colors.white};
   cursor: pointer;
@@ -34,6 +34,9 @@ const StyledLink = styled.a`
     font-size: ${({ theme }) => theme.fontSize.l};
     margin: 6px 2px;
   }
+
+  ${({ active, theme }) =>
+    active && `border-bottom: 2px solid ${theme.colors.js}`};
 `;
 
 const IconWrapper = styled.div`

c430a821ca769393c4c338a2a361aae9db6151f4
Maciej Szostak <maciek.szostak55@gmail.com>
2023-11-14 13:59:10 +0100

    Install tailwindcss and configure basic routing

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div className="flex flex-col items-center justify-center w-screen h-screen">
+    <p className="text-purple-600 font-bold text-xl">Hello Nuxt!</p>
+    <div className="flex flex-col spce-y-2">
+      <NuxtLink to="/projects">Projects</NuxtLink>
+      <NuxtLink to="/tech">Tech</NuxtLink>
+    </div>
+  </div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,6 @@
+<template>
+  <div className="flex items-center justify-center w-screen h-screen">
+    <p className="text-purple-600 font-bold text-xl">Projects</p>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>

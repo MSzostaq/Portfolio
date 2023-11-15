68a64db7c5b2888748b43f8dadba631b5ceb94f9
Maciej Szostak <maciek.szostak55@gmail.com>
2023-11-15 19:12:24 +0100

    Create error page

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-[100dvh]">
+    <p class="text-xl font-bold">{{ error.statusCode }}</p>
+    <p class="text-xl font-bold">{{ error.message }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(["error"]);
+</script>

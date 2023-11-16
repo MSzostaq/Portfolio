45cb338e0cac1408e51c0c48632036b19794b56b
Maciej Szostak <maciek.szostak55@gmail.com>
2023-11-16 15:24:32 +0100

    Add back button to error page

--- error.vue
+++ error.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex flex-col items-center justify-center h-[100dvh]">
     <p class="text-xl font-bold">{{ error.statusCode }}</p>
-    <p class="text-xl font-bold">{{ error.message }}</p>
+    <button @click="handleError">Go home</button>
   </div>
 </template>
 
 <script setup>
 defineProps(["error"]);
+
+const handleError = () => clearError({ redirect: "/" });
 </script>

927753b03c1991d66e355754772005d2bede42d4
Maciej Szostak <maciek.szostak55@gmail.com>
2023-11-17 09:13:25 +0100

    Create `MobileMenu` button and update `Footer` content

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,5 @@
 <template>
-  <footer class="flex items-center justify-center p-4">
-    <div class="">
-      <p class="">Footer</p>
-    </div>
+  <footer class="flex items-center justify-center px-4 py-8">
+    <p class="font-light">© 2023 Maciej Szostak</p>
   </footer>
 </template>

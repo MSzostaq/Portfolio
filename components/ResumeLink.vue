f9c1c05f8cc68e5be9a6d2e94d0066352b577f91
Maciej Szostak <maciek.szostak55@gmail.com>
2023-12-05 10:26:21 +0100

    Change resume redirect url

--- components/ResumeLink.vue
+++ components/ResumeLink.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink
-    to="/docs/cv.pdf"
+    to="/docs/resume.pdf"
     target="_blank"
     class="flex items-center space-x-1 lg:hover:text-orange-500 lg:active:translate-y-0.5 lg:transition-all"
   >
